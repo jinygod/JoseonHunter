9072196e67f56e625ed6146a4203077cd52ed97d
JSJ <ayclub8866@naver.com>
2026-08-10 02:32:46 +0900

    fix: verify lobby lock prefab and shield icon

--- Assets/JoseonHunter/Tests/EditMode/PremiumLobbyAssetContractTests.cs
+++ Assets/JoseonHunter/Tests/EditMode/PremiumLobbyAssetContractTests.cs
@@ -22,47 +22,81 @@ namespace JoseonHunter.Tests.EditMode
             "Assets/JoseonHunter/Resources/UI/compact_weapon_slot.png";
         private const string LockSlashConstraintPath =
             "Assets/JoseonHunter/Scripts/Presentation/UI/LockSlashConstraint.cs";
-
-        [Test]
-        public void LockSlashConstraintIsAStandaloneSerializableMonoScript()
-        {
-            var script = AssetDatabase.LoadAssetAtPath<MonoScript>(LockSlashConstraintPath);
-
-            Assert.That(script, Is.Not.Null, LockSlashConstraintPath);
-            Assert.That(script.GetClass(), Is.Not.Null, "The lock slash component must be serializable on LobbyShell.prefab.");
-            Assert.That(script.GetClass().FullName, Is.EqualTo("JoseonHunter.Presentation.UI.LockSlashConstraint"));
-        }
+        private const string LockSlashConstraintGuid =
+            "27d3aac4fd2d4fce9b5ebdd62d8a41a1";
+        private const string DifficultyCardPrefabPath =
+            "Assets/JoseonHunter/Prefabs/UI/Lobby/Modules/DifficultyCard.prefab";
 
         [TestCase(720f, 1280f)]
         [TestCase(1080f, 2340f)]
-        public void LockedDifficultyDecorationStaysInsidePortraitCard(float width, float height)
+        public void DifficultyCardPrefabKeepsSerializedLockDecorationInsideAuthoredCard(
+            float width,
+            float height)
         {
-            var root = new GameObject("Portrait Canvas", typeof(RectTransform), typeof(Canvas));
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(DifficultyCardPrefabPath);
+            var expectedScript = AssetDatabase.LoadAssetAtPath<MonoScript>(LockSlashConstraintPath);
+
+            Assert.That(prefab, Is.Not.Null, DifficultyCardPrefabPath);
+            Assert.That(expectedScript, Is.Not.Null, LockSlashConstraintPath);
+            Assert.That(expectedScript.GetClass(), Is.EqualTo(typeof(LockSlashConstraint)));
+            Assert.That(AssetDatabase.AssetPathToGUID(LockSlashConstraintPath),
+                Is.EqualTo(LockSlashConstraintGuid));
+            Assert.That(prefab.GetComponentsInChildren<LockSlashConstraint>(true).Length,
+                Is.EqualTo(1));
+            Assert.That(prefab.GetComponent<RectTransform>().sizeDelta,
+                Is.EqualTo(new Vector2(280f, 100f)));
+
+            var canvasObject = new GameObject(
+                "Portrait Canvas",
+                typeof(RectTransform),
+                typeof(Canvas));
             try
             {
-                var canvasRect = root.GetComponent<RectTransform>();
+                var canvas = canvasObject.GetComponent<Canvas>();
+                canvas.renderMode = RenderMode.WorldSpace;
+                var canvasRect = canvasObject.GetComponent<RectTransform>();
                 canvasRect.sizeDelta = new Vector2(width, height);
 
-                var cardObject = new GameObject(
-                    "Difficulty Card",
-                    typeof(RectTransform),
-                    typeof(CanvasRenderer),
-                    typeof(Image),
-                    typeof(Button));
-                cardObject.transform.SetParent(root.transform, false);
-                var cardRect = cardObject.GetComponent<RectTransform>();
-                cardRect.sizeDelta = new Vector2(width - 64f, 112f);
-                var button = cardObject.GetComponent<Button>();
-                button.targetGraphic = cardObject.GetComponent<Image>();
+                var instance = PrefabUtility.InstantiatePrefab(
+                    prefab,
+                    canvasObject.transform) as GameObject;
+                Assert.That(instance, Is.Not.Null, DifficultyCardPrefabPath);
 
-                PremiumPixelUiSkin.ApplyDifficulty(button, selected: false, locked: true);
+                var cardRect = instance.GetComponent<RectTransform>();
+                var buttonTransform = instance.transform.Find("Button");
+                var button = buttonTransform?.GetComponent<Button>();
+                var buttonRect = buttonTransform as RectTransform;
+                Assert.That(cardRect.sizeDelta, Is.EqualTo(new Vector2(280f, 100f)));
+                Assert.That(button, Is.Not.Null, "DifficultyCard.prefab must retain its authored Button.");
+                Assert.That(buttonRect, Is.Not.Null);
+                Assert.That(buttonRect.anchorMin, Is.EqualTo(Vector2.zero));
+                Assert.That(buttonRect.anchorMax, Is.EqualTo(Vector2.one));
+                Assert.That(buttonRect.sizeDelta, Is.EqualTo(Vector2.zero));
 
-                AssertRectInside(cardObject.transform.Find("Lock Slash").GetComponent<RectTransform>(), cardRect);
-                AssertRectInside(cardObject.transform.Find("Lock Icon").GetComponent<RectTransform>(), cardRect);
+                var constraint = instance
+                    .GetComponentsInChildren<LockSlashConstraint>(true)
+                    .Single();
+                var actualScript = MonoScript.FromMonoBehaviour(constraint);
+                Assert.That(actualScript, Is.SameAs(expectedScript),
+                    "DifficultyCard.prefab must serialize the standalone lock constraint script.");
+                Assert.That(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(actualScript)),
+                    Is.EqualTo(LockSlashConstraintGuid));
+
+                constraint.Configure();
+                Canvas.ForceUpdateCanvases();
+
+                var slashRect = constraint.GetComponent<RectTransform>();
+                var lockIconRect = buttonTransform.Find("Lock Icon") as RectTransform;
+                Assert.That(slashRect, Is.Not.Null);
+                Assert.That(lockIconRect, Is.Not.Null);
+                Assert.That(buttonRect.rect.size, Is.EqualTo(new Vector2(280f, 100f)));
+
+                AssertRectInside(slashRect, buttonRect);
+                AssertRectInside(lockIconRect, buttonRect);
             }
             finally
             {
-                Object.DestroyImmediate(root);
+                Object.DestroyImmediate(canvasObject);
             }
         }
 
